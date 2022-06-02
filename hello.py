988ebde824e72349ac9d07a5c22bf2b9e6499ec5
theanxiuser <bishalpoudel607@gmail.com>
2022-06-02 22:20:03 +0545

    changes into url decor

--- hello.py
+++ hello.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def index():
+    return "Index Page"
+
+@app.route("/hello")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "Hello World!"
 
 @app.route("/<name>")
 def hello(name):
