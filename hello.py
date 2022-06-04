741917564acf8f508d9f043bcb7302c850960b0d
theanxiuser <bishalpoudel607@gmail.com>
2022-06-04 22:07:50 +0545

    variable rule in routing

--- hello.py
+++ hello.py
@@ -17,3 +17,18 @@ def hello_world():
 @app.route("/<name>")
 def hello(name):
     return f"Hello, {escape(name)}!"
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    # show the user profile for that user
+    return f"User {escape(username)}"
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    # show the post with the given id
+    return f"Post {post_id}"
+
+@app.route("/path/<path:subpath>")
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f"Subpath {escape(subpath)}"
